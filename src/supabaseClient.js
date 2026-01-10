6fa06ac77c7eb479e4aa9285ed0d2bd6b5e814aa
DamieMegah <dareyjustice@gmail.com>
2026-01-10 16:27:58 +0100

    upload supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ctbeqcwfhowpvnafedzn.supabase.co'
-
-const supabaseKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0YmVxY3dmaG93cHZuYWZlZHpuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1NjU2NDk2NywiZXhwIjoyMDcyMTQwOTY3fQ.ElxuWIb1Df2n2Wk_CxQt5xV3PlwbgrObU0B5RNJA6S4";
+//===key is locked in this folder try cracking it using 10 numbers pi and institute g--- comit-- xd-inpy install
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
